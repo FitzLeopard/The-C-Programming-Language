14ab02ec7fb9075e37628d1515a0a27567a484a5
FitzLeopold <1796229518@qq.com>
2018-05-30 18:22:19 +0800

    [+] Example Code on Page 27

---
+++ Chapter1/Example-Code/CallByValue.c
@@ -0,0 +1,34 @@
+/*
+ * 	Date: 2018-05-30
+ *	Page 27 of K&R
+ *	By Rivers
+*/
+
+#include<stdio.h>
+
+int power(int m, int n);
+
+/* test power function */
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; ++i) {
+		printf("%d %d %d\n", i, power(2, i), power(-3, i));
+	}
+
+	return 0;
+}
+
+/* power: raise base to n-th power; n >= 0;
+ * Utilize property---Call By Value		  */
+int power(int base, int n)
+{
+	int p;
+
+	for (p = 1; n > 0; --n) {
+		p *= base;
+	}
+
+	return p;
+}
