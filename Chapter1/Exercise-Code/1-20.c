5a61a70e485e67ece81eadb232c51ff920a6a24c
FitzLeopold <1796229518@qq.com>
2018-06-01 11:11:15 +0800

    [+] Code for Exercise 1-20

---
+++ Chapter1/Exercise-Code/1-20.c
@@ -0,0 +1,38 @@
+/*
+ *	Date: 2018-06-01
+ *	Exercise 1-20 of K&R
+ *	By Rivers
+*/
+
+#include<stdio.h>
+
+#define TABLEN 4
+
+int main()
+{
+	int len;		/* the length of all the charcters seen so far
+					 * in the current line.						*/
+	int next;		/* the next place for the blank to replace the tab */
+	int c;
+
+	len = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			next = (len / 4 + 1) * 4;
+			for (; len < next; ++len) {
+				putchar(' ');
+			}
+		}
+		else {
+			putchar(c);
+			if (c == '\n') {
+				len = 0;
+			}
+			else {
+				++len;
+			}
+		}
+	}
+
+	return 0;
+}
