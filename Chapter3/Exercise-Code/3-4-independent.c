f65887863072fb4da06cae3b5bf3a3d8b6ed1a68
FitzLeopold <1796229518@qq.com>
2018-06-10 11:46:20 +0800

    [+] Code for Exercise 3-4

---
+++ Chapter3/Exercise-Code/3-4-independent.c
@@ -0,0 +1,68 @@
+/*
+ *	Date: 2018-06-10
+ *	Exercise 3-4 of K&R
+ *	By Rivers
+*/
+
+#include<stdio.h>
+#include<limits.h>
+#include<string.h>
+
+#define MAX 1000
+
+/* itoa: convert integer n into string s, version 2
+ * INT_MIN issue fixed							  */
+void itoa(int n, char s[]);
+/* reverse: reverse s in place */
+void reverse(char s[]);
+
+int main()
+{
+	char s[MAX];
+	int n;
+
+	itoa(INT_MIN, s);
+	printf("int min: %d is converted to %s.\n", INT_MIN, s);
+
+	printf("Enter a number:\n");
+	scanf("%d", &n);
+	itoa(n, s);
+	printf("%s\n", s);
+
+	return 0;
+}
+
+/* itoa: convert integer n into string s, version 2
+ * INT_MIN issue fixed							  */
+void itoa(int n, char s[])
+{
+	int sign = n, i = 0;
+
+	if (sign < 0) {
+		s[i++] = n / 10 * 10 - n + '0';
+		n /= 10;
+		n = -n;
+	}/* Get the last number in n so that 
+	  * n = -n will not cause overflow */
+	else {
+		s[i++] = n - n / 10 * 10 + '0';
+		n /= 10;
+	}			
+	do {
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) != 0);
+	if (sign < 0) {
+		s[i++] = '-';
+	}
+	s[i] = '\0';
+	reverse(s);
+}
+
+/* reverse: reverse s in place */
+void reverse(char s[])
+{
+	int i, j, c;
+
+	for (i = 0, j = strlen(s) - 1; j > i; ++i, --j)
+		c = s[i], s[i] = s[j], s[j] = c;
+}
