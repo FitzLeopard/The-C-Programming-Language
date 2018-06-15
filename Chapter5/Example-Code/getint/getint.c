fbc76a414c34ad262a842e727a63011fdc976ec0
FitzLeopold <1796229518@qq.com>
2018-06-15 18:34:23 +0800

    [+] Example Code on Page 96

---
+++ Chapter5/Example-Code/getint/getint.c
@@ -0,0 +1,54 @@
+/*
+ *	Date: 2018-06-15
+ *	Page 97 of K&R
+ *	By Rivers
+*/
+
+#include<stdio.h>
+#include<ctype.h>
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];		/* buffer for ungetch */
+int bufp = 0;			/* next free position in buf */
+
+int getch(void) /* get a (possibly pushed back) char */
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)		/* push char back on input */
+{
+	if (bufp >= BUFSIZE) {
+		printf("Error: ungetch: too many characters\n");
+	}
+	else {
+		buf[bufp++] = c;
+	}
+}
+
+/* getint: get next integer from input into *pn */
+int getint(int *pn)
+{
+	int c, sign;
+
+	while (isspace(c = getch()))
+		;
+	if (!isdigit(c) && c != EOF && c != '-' && c != '+') {
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '-' || c == '+') {
+		c = getch();
+	}
+	for (*pn = 0; isdigit(c); c = getch()) {
+		*pn = *pn * 10 + c - '0';
+	}
+	*pn *= sign;
+	if (c != EOF) {
+		ungetch(c);
+	}
+	return c;
+}
+
