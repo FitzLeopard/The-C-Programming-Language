617e9226ae61a3048f7fd5f09735a0727a6cdc96
FitzLeopold <1796229518@qq.com>
2018-06-15 18:51:13 +0800

    [+] Code for Exercise 5-1

---
+++ Chapter5/Exercise-Code/5-1/5-1.c
@@ -0,0 +1,31 @@
+/*
+ *	Date: 2018-06-15
+ *	Exercise 5-1 of K&R
+ *	By Rivers
+*/
+
+#include<stdio.h>
+
+#define SIZE 100
+
+int getint(int * pn);
+
+int main()
+{
+	int z[SIZE], n, signal;
+
+	for (n = 0; n < SIZE && 
+			(signal = getint(&z[n])) != EOF &&
+			signal != 0; ++n)
+		;
+	if (signal == 0) {
+		printf("%d\n", n);
+		printf("There is a nonnumber term.\n");
+		return -1;
+	}
+	while (n--) {
+		printf("%d%c", z[n], (n % 10 == 9 || n == 0) ? '\n' : ' ');
+	}
+
+	return 0;
+}
