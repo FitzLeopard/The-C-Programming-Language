655a0f97aea52139828c93d4bb9019132e0f7a93
FitzLeopold <1796229518@qq.com>
2018-06-19 16:33:31 +0800

    [+] Example Code on Page 111

---
+++ Chapter5/Example-Code/MultiDimArray/dayMonth.c
@@ -0,0 +1,34 @@
+/*
+ *	Date: 2018-06-19
+ *	Page 111 of K&R
+ *	By Rivers
+*/
+
+static char daytab[2][13] = {
+	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+	int i, leap;
+
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	for (i = 1; i < month; ++i) {
+		day += daytab[leap][i];
+	}
+
+	return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+	int leap;
+
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	for (*pmonth = 1; yearday > daytab[leap][*pmonth]; ++*pmonth)
+		yearday -= daytab[leap][*pmonth];
+	*pday = yearday;
+}
